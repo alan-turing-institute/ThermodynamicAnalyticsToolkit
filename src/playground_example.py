61510609355ea7b9fb2d8e96e0338d6f721e7b83
Frederik Heber <frederik.heber@gmail.com>
2017-09-22 10:28:54 +0100

    No log events are written if no logdir is given.
    - this also speeds up learning quite a bit.

--- src/playground_example.py
+++ src/playground_example.py
@@ -43,6 +43,9 @@ def main(_):
         noise=FLAGS.noise,
         data_type=FLAGS.data_type)
 
+    LogSummaries = False
+    if FLAGS.log_dir != None:
+        LogSummaries = True
         
     print("Constructing neural network")
     nn=neuralnetwork()
@@ -53,10 +56,12 @@ def main(_):
 
     print("Starting session")
     sess = tf.Session()
-    nn.add_writers(sess, FLAGS.log_dir)
+    if LogSummaries:
+        nn.add_writers(sess, FLAGS.log_dir)
     nn.init_graph(sess)
-    nn.add_graphing_train()
-    nn.graph_truth(sess, input_data, input_labels, FLAGS.dimension)
+    if LogSummaries:
+        nn.add_graphing_train()
+        nn.graph_truth(sess, input_data, input_labels, FLAGS.dimension)
 
     def feed_dict(train, _data, _labels):
         """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
@@ -78,22 +83,25 @@ def main(_):
         "merged", "train_step"]))+[xinput]+list(map(lambda key: nn.get(key), ["y_", "y"]))
     train_nodes = list(map(lambda key: nn.get(key), [
         "merged", "train_step"]))
-    train_writer = nn.get("train_writer")
-    test_writer = nn.get("test_writer")
+    if LogSummaries:
+        train_writer = nn.get("train_writer")
+        test_writer = nn.get("test_writer")
     print("Starting to train")
     test_intervals = max(10, FLAGS.max_steps/100)
     summary_intervals = max(20,FLAGS.max_steps/10)
     for i in range(FLAGS.max_steps):
 #        print("Current training step is %d" % i)
-        if i % test_intervals == 0:  # Record summaries and test-set accuracy
+        if (i % test_intervals == 0):  # Record summaries and test-set accuracy
             summary, acc, ce, y_eval, yeval = sess.run(
                 testset_accuracy_nodes,
                 feed_dict=feed_dict(True, input_data, input_labels))
-            test_writer.add_summary(summary, i)
-            print('Accuracy at step %s: %s' % (i, acc))
-            #print('Cross-entropy at step %s: %s' % (i, ce))
-            print('y_ at step %s: %s' % (i, str(y_eval[0:9].transpose())))
-            print('y at step %s: %s' % (i, str(yeval[0:9].transpose())))
+            if LogSummaries:
+                test_writer.add_summary(summary, i)
+            if (i % test_intervals == 0):
+                print('Accuracy at step %s: %s' % (i, acc))
+                #print('Cross-entropy at step %s: %s' % (i, ce))
+                #print('y_ at step %s: %s' % (i, str(y_eval[0:9].transpose())))
+                #print('y at step %s: %s' % (i, str(yeval[0:9].transpose())))
         else:  # Record train set summaries, and train
             if i % summary_intervals == summary_intervals-1:  # Record execution stats
                 run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
@@ -103,22 +111,25 @@ def main(_):
                     feed_dict=feed_dict(False, input_data, input_labels),
                     options=run_options,
                     run_metadata=run_metadata)
-                train_writer.add_run_metadata(run_metadata, 'step%03d' % i)
-                train_writer.add_summary(summary, i)
+            	if LogSummaries:
+                	train_writer.add_run_metadata(run_metadata, 'step%03d' % i)
+                	train_writer.add_summary(summary, i)
 
-                plot_buf = nn.get_plot_buf(xinputeval, yeval, FLAGS.dimension)
-                plot_image_summary_ = sess.run(
-                    nn.get("plot_image_summary_train"),
-                    feed_dict={nn.get("plot_buf_train"): plot_buf.getvalue()})
-                train_writer.add_summary(plot_image_summary_, global_step=i)
-                print('Adding run metadata for', i)
+	                plot_buf = nn.get_plot_buf(xinputeval, yeval, FLAGS.dimension)
+        	        plot_image_summary_ = sess.run(
+                	    nn.get("plot_image_summary_train"),
+	                    feed_dict={nn.get("plot_buf_train"): plot_buf.getvalue()})
+        	        train_writer.add_summary(plot_image_summary_, global_step=i)
+                	print('Adding run metadata for', i)
             else:  # Record a summary
                 summary, _ = sess.run(
                     train_nodes,
                     feed_dict=feed_dict(False, input_data, input_labels))
-                train_writer.add_summary(summary, i)
-    train_writer.close()
-    test_writer.close()
+            	if LogSummaries:
+                	train_writer.add_summary(summary, i)
+    if LogSummaries:
+        train_writer.close()
+        test_writer.close()
     print("TRAINED.")
 
 if __name__ == '__main__':
@@ -140,8 +151,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--log_dir',
         type=str,
-        default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
-             'tensorflow/Playground/logs/playground_example_with_summaries'),
+        default=None,
         help='Summaries log directory')
     FLAGS, unparsed = parser.parse_known_args()
 tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
