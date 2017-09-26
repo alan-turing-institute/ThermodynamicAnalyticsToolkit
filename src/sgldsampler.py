345fdd6270c87c9105715a78de818e72ab23c65d
Frederik Heber <frederik.heber@gmail.com>
2017-09-26 13:52:55 +0100

    We added normally distributed noise to the SGLDOptimizer.
    - this is really a nasty hack as we generate the noise via numpy.random.normal,
      place it in a slot and then use in the final assign_ops statement.
    - I am not sure whether the learning_rate will get properly evaluated

--- src/sgldsampler.py
+++ src/sgldsampler.py
@@ -5,6 +5,7 @@ from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import state_ops
 from tensorflow.python.framework import ops
 from tensorflow.python.training import optimizer
+import tensorflow as tf
 import numpy as np
 
 class SGLDSampler(optimizer.Optimizer):
@@ -19,10 +20,23 @@ class SGLDSampler(optimizer.Optimizer):
         self._lr_t = ops.convert_to_tensor(self._lr, name="learning_rate")
     
     def _create_slots(self, var_list):
-        pass
+        for v in var_list:
+            self._zeros_slot(v, "noise", self._name)
     
     def _apply_dense(self, grad, var):
         lr_t = math_ops.cast(self._lr_t, var.dtype.base_dtype)
+        #print("lr_t is "+str(self._lr))
+        noise = self.get_slot(var, "noise")
+        print("noise has shape "+str(noise.get_shape()))
+        no_vars = np.cumprod(noise.get_shape())[-1]
+        random_noise = tf.reshape(
+            math_ops.cast(np.random.normal(loc=0,scale=self._lr, size=no_vars),
+                          var.dtype.base_dtype), noise.get_shape())
+        print("random_noise has shape "+str(random_noise.get_shape()))        
+        noise_t = noise.assign(random_noise)
         
-        var_update = state_ops.assign_sub(var, lr_t * grad)
+        var_update = state_ops.assign_sub(var, lr_t * grad + noise_t)
         return control_flow_ops.group(*[var_update])
+
+    def _apply_sparse(self, grad, var):
+        raise NotImplementedError("Sparse gradient updates are not supported.")
