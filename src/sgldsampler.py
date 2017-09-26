1b6ff22705b2b594cab6db9a579bbac94a458c9f
Frederik Heber <frederik.heber@gmail.com>
2017-09-26 12:20:37 +0100

    Another way round, re-implemented GradientDescent in SGLDOptimizer.
    - it seems one can use Python to implement a new optimizer. However, the true
      operations are actually C++ OpKernels.
    - Hence, this is just for testing.

--- src/sgldsampler.py
+++ src/sgldsampler.py
@@ -1,27 +1,28 @@
-import tensorflow as tf
+# This is heavily inspired by  https://github.com/openai/iaf/blob/master/tf_utils/adamax.py
 
-class SGLDSampler(tf.train.GradientDescentOptimizer):
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import state_ops
+from tensorflow.python.framework import ops
+from tensorflow.python.training import optimizer
+import numpy as np
+
+class SGLDSampler(optimizer.Optimizer):
     ''' implements a Stochastic Gradient Langevin Dynamics Sampler
     in the form of a TensorFlow Optimizer
     '''
-    def __init__(self, rate):
-        super(SGLDSampler, self).__init__(rate)
+    def __init__(self, learning_rate, use_locking=False, name='SGLDSampler'):
+        super(SGLDSampler, self).__init__(use_locking, name)
+        self._lr = learning_rate
     
-    def get_name(self):
-        return str("SGLDSampler")
+    def _prepare(self):
+        self._lr_t = ops.convert_to_tensor(self._lr, name="learning_rate")
     
-    def minimize(self,
-                 loss,
-                 global_step=None,
-                 var_list=None,
-                 gate_gradients=tf.train.Optimizer.GATE_OP,
-                 aggregation_method=None,
-                 collocate_gradients_with_ops=False,
-                 name=None,
-                 grad_loss=None):
-        return super(SGLDSampler, self).minimize(
-            loss, global_step, var_list, gate_gradients,
-            aggregation_method, collocate_gradients_with_ops,
-            name, grad_loss)
+    def _create_slots(self, var_list):
+        pass
+    
+    def _apply_dense(self, grad, var):
+        lr_t = math_ops.cast(self._lr_t, var.dtype.base_dtype)
         
-    
+        var_update = state_ops.assign_sub(var, lr_t * grad)
+        return control_flow_ops.group(*[var_update])
