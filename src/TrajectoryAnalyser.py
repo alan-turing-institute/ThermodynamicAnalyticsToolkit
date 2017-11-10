6cce6e2870cca2bcf69f76488b2dcf473729ac0e
Frederik Heber <frederik.heber@gmail.com>
2017-11-10 14:39:22 +0000

    DDSAnalyser averages over both run and trajectory files separately.

--- src/TrajectoryAnalyser.py
+++ src/TrajectoryAnalyser.py
@@ -15,16 +15,22 @@ def parse_parameters():
     """
     parser = argparse.ArgumentParser()
     # please adhere to alphabetical ordering
+    parser.add_argument('--average_run_file', type=str, default=None,
+        help='CSV file name to output averages and variances of energies.')
+    parser.add_argument('--average_trajectory_file', type=str, default=None,
+        help='CSV file name to output averages and variances of all degrees of freedom.')
     parser.add_argument('--drop_burnin', type=int, default=0,
         help='How many values to drop at the beginning of the trajectory.')
     parser.add_argument('--csv_file', type=str, default=None,
         help='CSV run file name to read run time values from.')
     parser.add_argument('--every_nth', type=int, default=1,
         help='Evaluate only every nth trajectory point to files, e.g. 10')
-    parser.add_argument('--output_file', type=str, default=None,
-        help='CSV file name to output averages and variances.')
+    parser.add_argument('--run_file', type=str, default=None,
+        help='CSV run file name to read run time values from.')
     parser.add_argument('--steps', type=int, default=20,
         help='How many evaluation steps to take')
+    parser.add_argument('--trajectory_file', type=str, default=None,
+        help='CSV trajectory file name to read trajectories from and compute diffusion maps on.')
     parser.add_argument('--version', '-V', action="store_true",
         help='Gives version information')
     return parser.parse_known_args()
