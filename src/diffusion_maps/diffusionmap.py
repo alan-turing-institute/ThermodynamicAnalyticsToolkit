0fb8cbcb3254040eceb6944b75f86cfed6d338f1
Frederik Heber <frederik.heber@gmail.com>
2017-11-13 11:28:47 +0000

    DOCU: Added documentation to every function in diffusionmap module.

--- src/diffusion_maps/diffusionmap.py
+++ src/diffusion_maps/diffusionmap.py
@@ -20,6 +20,12 @@ from numpy import linalg as LA
 
 
 def compute_kernel(X, epsilon):
+    """ Computes a sparse, gaussian kernel between points on a trajectory
+
+    :param X: vector with trajectories
+    :param epsilon: Determines the distance cutoff in nearest neighbor search
+    :return: sparse kernel matrix
+    """
 
     m = np.shape(X)[0]
 
@@ -37,36 +43,56 @@ def compute_kernel(X, epsilon):
     kernel = sps.csr_matrix((diffusion_kernel, albero.indices, albero.indptr), dtype = float, shape=(m,m))
     kernel = kernel + sps.identity(m)  # accounting for diagonal elements
 
-    return kernel;
+    return kernel
 
 
 def compute_VanillaDiffusionMap(kernel, X):
+    """ Rescales the original diffusion map by looking at the sparse kernel's diagonal.
 
-
+    :param kernel: kernel matrix
+    :param X: trajectory vector
+    :return: rescaled kernel matrix
+    """
     alpha = 0.5;
     m = np.shape(X)[0];
 
-    D = sps.csr_matrix.sum(kernel, axis=0);
+    D = sps.csr_matrix.sum(kernel, axis=0)
     Dalpha = sps.spdiags(np.power(D,-alpha), 0, m, m)
-    kernel = Dalpha * kernel * Dalpha;
+    kernel = Dalpha * kernel * Dalpha
 
-    D = sps.csr_matrix.sum(kernel, axis=0);
+    # normalize matrix
+    D = sps.csr_matrix.sum(kernel, axis=0)
     Dalpha = sps.spdiags(np.power(D,-1), 0, m, m)
-    kernel = Dalpha * kernel;
+    kernel = Dalpha * kernel
 
     return kernel
 
 
 def compute_target_distribution(number_of_steps, beta, loss):
+    """ Computes the target Boltzmann distribution
+
+    :param number_of_steps: number of trajectory steps
+    :param beta: inverse temperature parameter
+    :param loss: potential or loss function along trajectory
+    :return: target distribution (vector with number_of_steps)
+    """
     qTargetDistribution = np.zeros(number_of_steps)
 
     for i in range(0, number_of_steps):
         qTargetDistribution[i] = np.exp(-(loss[i] * beta))
+        #print("#"+str(i)+": Loss "+str(loss[i])+" beta "+str(beta)+": qTarget "+str(qTargetDistribution[i]))
 
     return qTargetDistribution
 
 
 def compute_TMDMap(X, epsilon, qTargetDistribution):
+    """ Computes the TM diffusion map variant.
+
+    :param X: trajectory vector
+    :param epsilon: cutoff parameter for constructing kernel matrix
+    :param qTargetDistribution: target Boltzmann distribution
+    :return:
+    """
 
     #print('Unweighting according to temperature '+repr(temperature))
     m = np.shape(X)[0];
@@ -75,12 +101,15 @@ def compute_TMDMap(X, epsilon, qTargetDistribution):
 
     qEmp=kernel.sum(axis=1)
 
+    # reweight matrix by ratio of target over empirical distribution
     weights = np.zeros(m)
     for i in range(0,len(X)):
-        weights[i] = np.sqrt(qTargetDistribution[i]) /  qEmp[i]
+        weights[i] = np.sqrt(qTargetDistribution[i]) / qEmp[i]
+    #print("Weights for targetted mean: "+str(weights[0:10]))
     D = sps.spdiags(weights, 0, m, m)
     Ktilde =  kernel * D
 
+    # normalize matrix
     Dalpha = sps.csr_matrix.sum(Ktilde, axis=0)
     Dtilde = sps.spdiags(np.power(Dalpha,-1), 0, m, m)
 
@@ -90,11 +119,26 @@ def compute_TMDMap(X, epsilon, qTargetDistribution):
 
 
 def pdist2(x,y):
+    """ Computes the euclidian distance for two vectors.
+
+    :param x: first vector
+    :param y: second vector
+    :return: euclidian distance between the two vectors
+    """
     v=np.sqrt(((x-y)**2).sum())
     return v
 
 
 def get_landmarks(data, K, q, V, energies):
+    """
+
+    :param data: trajectory vector
+    :param K: number of landmark points to obtain
+    :param q: empirial distribution
+    :param V: eigenvector (of diffusion map) to use
+    :param energies: loss or potential function
+    :return:
+    """
 
     m = float(q.size)
     #q=np.array(q)
@@ -157,6 +201,12 @@ def get_landmarks(data, K, q, V, energies):
 
 
 def sort_landmarks(data, landmarks):
+    """ Sort the landmark points according to ball_tree variant of nearest neighbor search.
+
+    :param data: trajectory
+    :param landmarks:  landmarks
+    :return: sorted landmarks
+    """
 
     X=data[landmarks,:]
 
@@ -171,6 +221,14 @@ def sort_landmarks(data, landmarks):
 
 
 def get_levelsets(data, K, q, V1):
+    """ Calculates the K level set points given a trajectory.
+
+    :param data: trajectory vector
+    :param K: number of level sets to calculate
+    :param q: empirial distribution
+    :param V1: eigenvector
+    :return: level set values
+    """
 
     m = float(q.size)
     #q=np.array(q)
@@ -213,6 +271,17 @@ def get_levelsets(data, K, q, V1):
 
 
 def computeFreeEnergyAtEveryPoint(X, V1, width, qTarget, qEmp, method='weighted'):
+    """
+
+    :param X: trajectory
+    :param V1: eigenvector
+    :param width: binning width
+    :param qTarget: target distribution
+    :param qEmp: empirial distribution
+    :param method: either weighted or raw (use weighted if sampled trajectories do
+           not follow the target distribution and need reweighting in order to do so)
+    :return: free energies calculated at all trajectory points
+    """
     if(method=='weighted'):
         weight, Ntilde = compute_weight_target_distribution(qTarget, qEmp)
 
@@ -246,6 +315,13 @@ def computeFreeEnergyAtEveryPoint(X, V1, width, qTarget, qEmp, method='weighted'
 
 
 def get_levelset_onePoint(idx, width, V1):
+    """ Return the levelset at a single point
+
+    :param idx: index of the desired point
+    :param width: width of the level set
+    :param V1: eigenvector
+    :return:
+    """
     #deltaMax=2*width
 
     #print(np.abs(V1 - V1[idx]))
@@ -257,21 +333,22 @@ def get_levelset_onePoint(idx, width, V1):
 
     levelset= tmp[0]
 
-
     return np.asarray(levelset)
 
 
-def compute_weight_target_distribution(target_distribution, qImoportanceSampling):
-    """
-    parameters: target_distribution and qImoportanceSampling have length of the trajectory x
+def compute_weight_target_distribution(target_distribution, qImportanceSampling):
+    """ Reweight the target distribution by another as obtained by importance sampling.
+
+    :param target_distribution: target distribution vector
+    :param qImportanceSampling: distribution use for reweighting
     """
 
     ModNr=1
 
-    weight=np.zeros(len(qImoportanceSampling))
+    weight=np.zeros(len(qImportanceSampling))
 
-    for count in range(len(qImoportanceSampling)):
-        weight[count]=target_distribution[count]/qImoportanceSampling[count]
+    for count in range(len(qImportanceSampling)):
+        weight[count]=target_distribution[count]/qImportanceSampling[count]
 
     Ntilde=np.sum(weight)
 
