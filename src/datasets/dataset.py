FIX: uses batches for gradient computation re-added.
- dataset has this capability for a long while now but it was dropped on
  accidentally on renaming an executable (when no longer had train and
  test but only train).
- removed shuffling of train set after new epoch to obtain same results on all
  tests at the moment.

src/datasets/dataset.py
@@ -71,7 +71,8 @@ class Dataset:
         """
         if self.epochStarted():
             # shuffle on start of epoch
-            self.shuffle_train()
+            #self.shuffle_train()
+            pass
         if self.batch_start + batch_size >= self.slice_index:
             # return rest
             batch_xs = self.xs[self.batch_start:self.slice_index]
@@ -79,7 +80,7 @@ class Dataset:
             remaining_size = batch_size-(self.slice_index-self.batch_start)
             if remaining_size != 0:
                 # reshuffle like on epoch restart
-                self.shuffle_train()
+                #self.shuffle_train()
                 # note that this copies the array and is not inplace!
                 batch_xs = np.append(batch_xs, self.xs[0:remaining_size])
                 batch_ys = np.append(batch_ys, self.ys[0:remaining_size])