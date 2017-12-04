2c8c543bd2865f86390d789ab4833af7c9877754
Frederik Heber <frederik.heber@gmail.com>
2017-12-04 23:42:33 +0000

    Runtime stores number of hidden layers and min and max number of nodes.
    - TESTS: sql database test additionally checks whether sqlite db is not empty.

--- src/runtime/runtime.py
+++ src/runtime/runtime.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+from DataDrivenSampler.common import get_list_from_string
+
 class runtime(object):
     """ This class contains runtime information and capability
     to write these to an sqlite file.
@@ -32,6 +34,9 @@ class runtime(object):
                     id INTEGER PRIMARY KEY,
                     batch_size INTEGER,
                     dimension INTEGER,
+                    hidden_num_layers INTEGER,
+                    hidden_min_nodes INTEGER,
+                    hidden_max_nodes INTEGER,
                     seed INTEGER,
                     step_width FLOAT,
                     init_time FLOAT,
@@ -44,12 +49,21 @@ class runtime(object):
                 # add values
                 add_values_format = """
                     INSERT INTO run_time
-                    (batch_size,dimension,seed,step_width,init_time,train_time, overall_time)
-                    VALUES ({batch_size}, {dimension}, {seed}, {step_width}, {init_time}, {train_time}, {overall_time});
+                    (batch_size,dimension,hidden_num_layers,hidden_min_nodes,hidden_max_nodes,seed,step_width,init_time,train_time, overall_time)
+                    VALUES ({batch_size}, {dimension}, {hidden_num_layers}, {hidden_min_nodes}, {hidden_max_nodes}, {seed}, {step_width}, {init_time}, {train_time}, {overall_time});
                 """
+                hidden_dimension = get_list_from_string(self.FLAGS.hidden_dimension)
+                min_nodes = 0
+                max_nodes = 0
+                if len(hidden_dimension) != 0:
+                    min_nodes = min(hidden_dimension)
+                    max_nodes = max(hidden_dimension)
                 add_values_command = add_values_format.format(
                     batch_size=self.FLAGS.batch_size,
                     dimension=self.FLAGS.dimension,
+                    hidden_num_layers=len(hidden_dimension),
+                    hidden_min_nodes=min_nodes,
+                    hidden_max_nodes=max_nodes,
                     seed=self.FLAGS.seed,
                     step_width=self.FLAGS.step_width,
                     init_time=self.time_init_network,
