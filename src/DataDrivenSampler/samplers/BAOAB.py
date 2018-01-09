FIX: all samplers and optimizers use control_dependencies to enforce virial, gradient, var_update execution order.
- execution order is not fixed with tensorflow if nodes do not directly depend
  on another. This caused virials to change slightly when var_update actually
  was executed before the virial evaluation.
  We now make sure that virials and gradients are evaluated before the variable
  update takes place. This is done over all samplers and optimizers.
- TESTS: HMC regression tests were not stable with respect to this. A single
  value flipped and changed the diff'ed against file. These have been replaced.
- Note that all other samplers were constructed already such that there we no
  changes.
- TESTS: Optimizers regression tests (Datasets) however have changed. We have
  checked the values with control_dependencies with tf 1.3 and tf 1.4 and they
  coincide (if using the statement). Hence, we take over the newly obtained
  files to diff against assuming that the old ones were broken because virials
  were evaluated in wrong order (after variable update).

src/DataDrivenSampler/samplers/BAOAB.py
@@ -255,12 +255,15 @@ class BAOABSampler(GLAFirstOrderMomentumSampler):
         # next_qn = A(half_qn, tilde_half_pn, h / 2)
         position_full_step_t = position_half_step_t + 0.5 * step_width_t * momentum_noise_step_t
 
-        # assign parameters
-        var_update = state_ops.assign(var, position_full_step_t)
+        # make sure virial and gradients are evaluated before we update variables
+        with tf.control_dependencies([virial_global_t, gradient_global_t]):
+            # assign parameters
+            var_update = state_ops.assign(var, position_full_step_t)
 
         # assign moment to slot
         momentum_t = momentum.assign(momentum_noise_step_t)
 
+        # note: these are evaluated in any order, use control_dependencies if required
         return control_flow_ops.group(*[gradient_global_t, virial_global_t,
                                         noise_global_t, momentum_global_t,
                                         kinetic_energy_t,

src/DataDrivenSampler/samplers/GLAFirstOrderMomentumSampler.py
@@ -85,8 +85,10 @@ class GLAFirstOrderMomentumSampler(SGLDSampler):
         ub_repell, lb_repell = self._apply_prior(var)
         prior_force = step_width_t * (ub_repell + lb_repell)
 
-        # q=^{n+1} = q^n + M^{-1} p_{n+1} ∆t
-        var_update = state_ops.assign_add(var, step_width_t * momentum_full_step_t - prior_force)
+        # make sure virial and gradients are evaluated before we update variables
+        with tf.control_dependencies([virial_global_t, gradient_global_t]):
+            # q=^{n+1} = q^n + M^{-1} p_{n+1} ∆t
+            var_update = state_ops.assign_add(var, step_width_t * momentum_full_step_t - prior_force)
 
         alpha_t = tf.exp(-friction_constant_t * step_width_t)
 
@@ -107,6 +109,7 @@ class GLAFirstOrderMomentumSampler(SGLDSampler):
             kinetic_energy = tf.get_variable("kinetic", dtype=tf.float64)
             kinetic_energy_t = tf.assign_add(kinetic_energy, momentum_sq)
 
+        # note: these are evaluated in any order, use control_dependencies if required
         return control_flow_ops.group(*[gradient_global_t, virial_global_t, var_update,
                                         noise_global_t, momentum_t, momentum_global_t,
                                         kinetic_energy_t])

src/DataDrivenSampler/samplers/GLASecondOrderMomentumSampler.py
@@ -79,8 +79,10 @@ class GLASecondOrderMomentumSampler(GLAFirstOrderMomentumSampler):
         ub_repell, lb_repell = self._apply_prior(var)
         prior_force = step_width_t * (ub_repell + lb_repell)
 
-        # q=^{n+1} = q^n + M^{-1} p_{n+1/2} ∆t
-        var_update = state_ops.assign_add(var, step_width_t * momentum_t - prior_force)
+        # make sure virial and gradients are evaluated before we update variables
+        with tf.control_dependencies([virial_global_t, gradient_global_t]):
+            # q=^{n+1} = q^n + M^{-1} p_{n+1/2} ∆t
+            var_update = state_ops.assign_add(var, step_width_t * momentum_t - prior_force)
 
         # p^{n+1} = \alpha_{\Delta t} p^{n+1} + \sqrt{ \frac{1-\alpha^2_{\Delta t}}{\beta} M } G^n
         with tf.variable_scope("accumulate", reuse=True):
@@ -93,6 +95,7 @@ class GLASecondOrderMomentumSampler(GLAFirstOrderMomentumSampler):
             kinetic_energy = tf.get_variable("kinetic", dtype=tf.float64)
             kinetic_energy_t = tf.assign_add(kinetic_energy, momentum_sq)
 
+        # note: these are evaluated in any order, use control_dependencies if required
         return control_flow_ops.group(*[gradient_global_t, virial_global_t, var_update,
                                         noise_global_t, momentum_t, momentum_global_t,
                                         kinetic_energy_t])

src/DataDrivenSampler/samplers/GradientDescent.py
@@ -39,5 +39,10 @@ class GradientDescent(tf.train.GradientDescentOptimizer):
             gradient_global_t = tf.assign_add(gradient_global, tf.reduce_sum(tf.multiply(scaled_gradient, scaled_gradient)))
             virial_global = tf.get_variable("virials", dtype=tf.float64)
             virial_global_t = tf.assign_add(virial_global, tf.reduce_sum(tf.multiply(grad, var)))
-        control_group_gradient_descent_t = super(GradientDescent, self)._apply_dense(grad, var)
+
+        # make sure virial and gradients are evaluated before we update variables
+        with tf.control_dependencies([virial_global_t, gradient_global_t]):
+            control_group_gradient_descent_t = super(GradientDescent, self)._apply_dense(grad, var)
+
+        # note: these are evaluated in any order, use control_dependencies if required
         return control_flow_ops.group(*[virial_global_t, control_group_gradient_descent_t, gradient_global_t])

src/DataDrivenSampler/samplers/HamiltonianMonteCarloSampler.py
@@ -190,10 +190,13 @@ class HamiltonianMonteCarloSampler(SGLDSampler):
             with tf.control_dependencies([state_ops.assign_add(var, scaled_momentum)]):
                 return tf.identity(old_total_energy_t)
 
-        criterion_block_t = tf.cond(
-            tf.equal(tf.mod(current_step_t, num_steps_t), 0),
-            accept_reject_block, step_block)
+        # make sure virial and gradients are evaluated before we update variables
+        with tf.control_dependencies([virial_global_t, gradient_global_t]):
+            criterion_block_t = tf.cond(
+                tf.equal(tf.mod(current_step_t, num_steps_t), 0),
+                accept_reject_block, step_block)
 
+        # note: these are evaluated in any order, use control_dependencies if required
         return control_flow_ops.group(*([momentum_criterion_block_t, criterion_block_t,
                                         virial_global_t, gradient_global_t,
                                         momentum_global_t, kinetic_energy_t]))

src/DataDrivenSampler/samplers/sgldsampler.py
@@ -191,7 +191,12 @@ class SGLDSampler(optimizer.Optimizer):
         # prior force act directly on var
         ub_repell, lb_repell = self._apply_prior(var)
         prior_force = step_width_t * (ub_repell + lb_repell)
-        var_update = state_ops.assign_sub(var, scaled_gradient + scaled_noise + prior_force)
+
+        # make sure virial and gradients are evaluated before we update variables
+        with tf.control_dependencies([virial_global_t, gradient_global_t, noise_global_t]):
+            var_update = state_ops.assign_sub(var, scaled_gradient + scaled_noise + prior_force)
+
+        # note: these are evaluated in any order, use control_dependencies if required
         return control_flow_ops.group(*[virial_global_t, var_update, gradient_global_t, noise_global_t])
 
     def _apply_sparse(self, grad, var):