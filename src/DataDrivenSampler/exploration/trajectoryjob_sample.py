FIX: Exploration did not correctly write away the averages file so far.
- this did not appear because each sample job so far appended to it. Now, as
  we have train jobs that do not share all the averages of the sample jobs,
  some columns were underrepresented.

src/DataDrivenSampler/exploration/trajectoryjob_sample.py
@@ -54,14 +54,14 @@ class TrajectoryJob_sample(TrajectoryJob):
 
         # run graph here
         self.network_model.reset_dataset()
-        run_info, trajectory, _ = self.network_model.sample(
-            return_run_info=True, return_trajectories=True, return_averages=False)
+        run_info, trajectory, averages = self.network_model.sample(
+            return_run_info=True, return_trajectories=True, return_averages=True)
 
-        self._store_trajectory(_data, run_info, trajectory)
+        self._store_trajectory(_data, averages, run_info, trajectory)
 
         return _data, self.continue_flag
 
-    def _store_trajectory(self, _data, run_info, trajectory):
+    def _store_trajectory(self, _data, averages, run_info, trajectory):
         steps = [int(i) for i in np.asarray(run_info.loc[:, 'step'])]
         losses = [float(i) for i in np.asarray(run_info.loc[:, 'loss'])]
         gradients = [float(i) for i in np.asarray(run_info.loc[:, 'scaled_gradient'])]
@@ -78,5 +78,6 @@ class TrajectoryJob_sample(TrajectoryJob):
                            _losses=losses,
                            _gradients=gradients,
                            _parameters=parameters,
+                           _averages_lines=averages,
                            _run_lines=run_info,
                            _trajectory_lines=trajectory)

src/DataDrivenSampler/exploration/trajectoryjob_train.py
@@ -28,7 +28,7 @@ class TrajectoryJob_train(TrajectoryJob_sample):
             continue_flag=continue_flag)
         self.job_type = "train"
 
-    def _store_last_step_of_trajectory(self, _data, run_info, trajectory):
+    def _store_last_step_of_trajectory(self, _data, averages, run_info, trajectory):
         step = [int(np.asarray(run_info.loc[:, 'step'])[-1])]
         loss = [float(np.asarray(run_info.loc[:, 'loss'])[-1])]
         gradient = [float(np.asarray(run_info.loc[:, 'scaled_gradient'])[-1])]
@@ -44,6 +44,7 @@ class TrajectoryJob_train(TrajectoryJob_sample):
                            _losses=loss,
                            _gradients=gradient,
                            _parameters=parameters,
+                           _averages_lines=averages,
                            _run_lines=run_info,
                            _trajectory_lines=trajectory)
 
@@ -69,10 +70,10 @@ class TrajectoryJob_train(TrajectoryJob_sample):
         # run graph here
         self.network_model.reset_dataset()
         try:
-            run_info, trajectory, _ = self.network_model.train(
-                return_run_info=True, return_trajectories=True, return_averages=False)
+            run_info, trajectory, averages = self.network_model.train(
+                return_run_info=True, return_trajectories=True, return_averages=True)
 
-            self._store_last_step_of_trajectory(_data, run_info, trajectory)
+            self._store_last_step_of_trajectory(_data, averages, run_info, trajectory)
 
         except errors_impl.InvalidArgumentError:
             logging.error("The trajectory diverged, aborting.")