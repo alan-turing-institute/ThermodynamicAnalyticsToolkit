7f1082239dba76d81d14168d983a1b873d8992b6
Frederik Heber <frederik.heber@gmail.com>
2018-03-01 01:32:32 +0100

    LossFunctionSampler accepts interval_center_file for specifying the interval's center point.
    - this is required when excluding parameters to give them a default value
      unequal to 0.
    - interval_center_file reads files containing w0 or weight0 or b0 or bias0.
    - TESTS: Adapted exclude_parameters test to use interval_centers_file.

--- doc/userguide/cmdline/lossfunctionsampler-exclude_parameters.sh
+++ doc/userguide/cmdline/lossfunctionsampler-exclude_parameters.sh
@@ -3,6 +3,8 @@ LossFunctionSampler \
   --batch_size 20 \
   --csv_file LossFunctionSampler-output-SGLD.csv \
   --exclude_parameters "w1" \
+  --interval_center_file centers.csv \
+  --interval_center_step 1 \
   --interval_weights -5 5 \
   --interval_biases -1 1 \
   --samples_weights 10 \
