fffb19e7226e0b4e2fbb24eda67458468cb43882
Frederik Heber <frederik.heber@gmail.com>
2018-03-17 14:32:12 +0000

    InputSpaceSampler uses model's assign_weights_and_biases functions.
    - dropped parameter trajectory_file from InputSpaceSampler.
    - DOCU: Adapted userguide example and text.
    - TESTFIX: dropped parameter replaced by parse_parameters_file.

--- doc/userguide/cmdline/inputspacesampler.sh
+++ doc/userguide/cmdline/inputspacesampler.sh
@@ -1,9 +1,9 @@
 InputSpaceSampler \
 	--batch_data_files grid.csv \
+	--csv_file InputSpaceSampler-output.csv \
 	--input_dimension 2 \
 	--interval_input -4 4 \
+	--parse_steps 1 \
+	--parse_parameters_file trajectory.csv \
 	--samples_input 10 \
-	--csv_file InputSpaceSampler-output.csv \
-	--seed 426 \
-	--steps 1 \
-	--trajectory_file trajectory.csv
+	--seed 426
