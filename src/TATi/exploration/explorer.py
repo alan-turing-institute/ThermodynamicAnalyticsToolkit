d33ea82e9bf310370162b9d770d10aa979dd9356
Frederik Heber <frederik.heber@gmail.com>
2018-04-11 14:44:24 +0200

    Refactord explorer to allow basin exploration.

--- src/TATi/exploration/explorer.py
+++ src/TATi/exploration/explorer.py
@@ -34,6 +34,7 @@ class Explorer(object):
             self.queue = TrajectoryJobQueue(max_legs, number_pruning)
         else:
             self.queue = TrajectoryProcessQueue(parameters, number_pruning, number_processes=use_processes, manager=manager)
+        self.max_distance = 0. # helps to estimate when exploration has finished
 
     def add_used_data_ids_list(self, _list):
         """ Pass function through to TrajectoryQueue
@@ -65,24 +66,38 @@ class Explorer(object):
         """
         # d. spawn new trajectories from these points
         cornerpoints = []
-        data_container = self.queue.get_data_container()
         for i in range(len(idx_corner)):
-            logging.debug("Current corner point is (first and last five shown):" \
-                          +str(parameters[idx_corner[i]][:5])+" ... "+str(parameters[idx_corner[i]][-5:]))
-            current_id = data_container.add_empty_data(type="sample")
-            data_object = data_container.get_data(current_id)
-            data_object.steps[:] = [steps[idx_corner[i]]]
-            data_object.parameters[:] = [parameters[idx_corner[i]]]
-            data_object.losses[:] = [losses[idx_corner[i]]]
-            data_object.gradients[:] = [1]
-            data_container.update_data(data_object)
-
-            self.queue.add_sample_job(
-                data_object=data_object,
-                run_object=network_model,
-                continue_flag=True)
-            cornerpoints.append( [data_object.steps[-1], data_object.losses[-1], data_object.parameters[-1]] )
+            self.spawn_new_trajectory(steps[idx_corner[i]], parameters[idx_corner[i]],
+                                      losses[idx_corner[i]], 1, network_model)
+            cornerpoints.append( [steps[idx_corner[i]], losses[idx_corner[i]], parameters[idx_corner[i]]] )
         return cornerpoints
+
+    def spawn_new_trajectory(self, step, parameters, loss, gradient, network_model):
+        """ Run further trajectories for a given list of corner points.
+
+        :param steps: continuous step number per step
+        :param parameters: trajectory as parameters (i.e. weights and biases) per step
+        :param losses: loss per step
+        :param idx_corner: list of indices of all corner points w.r.t. trajectory
+        :param network_model: model of neural network with Session for sample and optimize jobs
+        :return: added cornerpoints as array
+        """
+        # d. spawn new trajectories from these points
+        data_container = self.queue.get_data_container()
+        logging.debug("Starting new trajectory from (first and last five shown):" \
+                      +str(parameters[:5])+" ... "+str(parameters[-5:]))
+        current_id = data_container.add_empty_data(type="sample")
+        data_object = data_container.get_data(current_id)
+        data_object.steps[:] = [step]
+        data_object.parameters[:] = [parameters]
+        data_object.losses[:] = [loss]
+        data_object.gradients[:] = [gradient]
+        data_container.update_data(data_object)
+
+        self.queue.add_sample_job(
+            data_object=data_object,
+            run_object=network_model,
+            continue_flag=True)
 
     def combine_sampled_trajectories(self):
         """ Combines all trajectories contained in the internal container.
@@ -141,15 +156,13 @@ class Explorer(object):
 
         return idx_corner
 
-    def get_corner_points(self, trajectory, losses, parameters,
-                          number_of_corner_points):
-        """ Returns the corner points for a given
+    def perform_diffusion_map_analysis(self, trajectory, losses, parameters):
+        """ Returns the eigenvectors and eigenvalues of the diffusion map
 
         :param trajectory: trajectory as parameters (i.e. weights and biases) per step
         :param losses: loss per step
         :param parameters: parameter struct controlling the diffusion map analysis
-        :param number_of_corner_points: number of corner points to return
-        :return: list of indices of the corner points with respect to the given trajectory
+        :return: eigenvectors, eigenvalues
         """
         try:
             dmap_eigenvectors, dmap_eigenvalues, dmap_kernel = compute_diffusion_maps( \
@@ -163,15 +176,9 @@ class Explorer(object):
             logging.error(": Vectors were non-convergent.")
             dmap_eigenvectors = np.zeros( (np.shape(trajectory)[0], parameters.number_of_eigenvalues) )
             dmap_eigenvalues = np.zeros( (parameters.number_of_eigenvalues) )
-            dmap_kernel = np.zeros( (np.shape(trajectory)[0], np.shape(trajectory)[0]) )
-            # override landmarks to skip computation
-            parameters.landmarks = 0
-        logging.info("Global diffusion map eigenvalues: "+str(dmap_eigenvalues))
+            #dmap_kernel = np.zeros( (np.shape(trajectory)[0], np.shape(trajectory)[0]) )
 
-        # c. find number of points maximally apart
-        idx_corner = self.find_corner_points(
-                dmap_eigenvectors, number_of_corner_points)
-        return idx_corner
+        return dmap_eigenvectors, dmap_eigenvalues
 
     def run_all_jobs(self, network_model, parameters):
         """ Run all jobs currently found in the TrajectoryJob queue.
@@ -213,3 +220,78 @@ class Explorer(object):
                     trajectory.append(trajectory_line)
 
         return run_info, trajectory
+
+    def _get_largest_separation_distance(self, dmap_eigenvectors):
+        """ Helper function to get the largest separation distance over all
+        eigenvectors
+
+        :param dmap_eigenvectors: eigenvectors to look for changes
+        :return: largest distance, i.e. maximum (max-min) value over all
+        """
+        distances = []
+        for i in range(np.shape(dmap_eigenvectors)[1]):
+            distances.append(np.argmax(dmap_eigenvectors[:, i]) - np.argmin(dmap_eigenvectors[:, i]))
+        return max(distances)
+
+    def has_exploration_finished(self, dmap_eigenvectors):
+        """ Checks whether the exploration in the current basin has finished.
+
+        We check this by looking at the diffusion map's eigenvector.
+        If the maximally separate distances per eigenvector has not
+        changed, then we have not made any progress and seem to have exhausted
+        the current basin.
+
+        :param dmap_eigenvectors: eigenvectors to look for changes
+        :return: true - has finished, false - exploration needs to continue
+        """
+        if dmap_eigenvectors is None:
+            return True
+        else:
+            new_distance = self._get_largest_separation_distance(dmap_eigenvectors)
+            logging.debug("New distance is "+str(new_distance)+", compared to old distance "
+                          +str(self.max_distance))
+            if new_distance > self.max_distance:
+                self.max_distance = new_distance
+                return False
+            else:
+                return True
+
+    def explore_basin(self, network_model):
+        """ This explores the current basin in the loss manifold fully at the
+        currently set temperature.
+
+        :param network_model: network_model used for sampling
+        :return: eigenvectors, eigenvalues of diffusion kernel of the sampled trajectory
+        """
+        dmap_eigenvectors = None
+        cornerpoints = []
+        self.max_distance = 0.
+        while True:
+            # a. combine all trajectories
+            steps, parameters, losses = self.combine_sampled_trajectories()
+
+            # b. perform diffusion map analysis
+            dmap_eigenvectors, dmap_eigenvalues = self.perform_diffusion_map_analysis(
+                parameters, losses, self.parameters)
+            if dmap_eigenvalues[0] == 0.:
+                # override landmarks to skip computation
+                parameters.landmarks = 0
+            logging.info("Global diffusion map eigenvalues: " + str(dmap_eigenvalues))
+
+            # check for exhausted exploration
+            if self.has_exploration_finished(dmap_eigenvectors):
+                break
+
+            # c. get corner points
+            idx_corner = self.find_corner_points(dmap_eigenvectors,
+                                                self.parameters.number_of_parallel_trajectories)
+
+            # d. spawn new trajectories from these points
+            cornerpoints.append(
+                self.spawn_corner_trajectories(steps, parameters, losses, idx_corner,
+                                               network_model))
+
+            # e. run all trajectories till terminated
+            self.run_all_jobs(network_model, self.parameters)
+
+        return steps, parameters, losses, dmap_eigenvectors, dmap_eigenvalues
