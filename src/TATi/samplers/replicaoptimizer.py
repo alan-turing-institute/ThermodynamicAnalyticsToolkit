ce120c44f4bdd02e40752ef1b7a2ee144d8e9879
Frederik Heber <frederik.heber@gmail.com>
2018-04-19 15:26:05 +0100

    Pulled in apply_dense() code from Optimizer class.
    - also needed some additional stuff as TF uses a processor class to handle
      different variable cases: dense, sparse, referenced variable, ...
    - we are now in the position to mix the gradients and pass them on properly
      to the deriving sampler.

--- src/TATi/samplers/replicaoptimizer.py
+++ src/TATi/samplers/replicaoptimizer.py
@@ -1,16 +1,62 @@
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
+from tensorflow.python.eager import context
 from tensorflow.python.ops import state_ops
 from tensorflow.python.framework import ops
-from tensorflow.python.training import optimizer
+from tensorflow.python.ops import resource_variable_ops
+from tensorflow.python.ops import variables
+from tensorflow.python.training.optimizer import Optimizer, _DenseResourceVariableProcessor, \
+    _OptimizableVariable, _StreamingModelPortProcessor, _get_variable_for
 import tensorflow as tf
 
 from TATi.models.basetype import dds_basetype
 
+class RefVariableReplicaProcessor(_OptimizableVariable):
+    """Processor for List of replicated Variable.
 
-class ReplicaOptimizer(optimizer.Optimizer):
+    This variable has multiple related gradients coming from the replicated
+    versions which might be used in the update scheme. This class overrides
+    the default `update_op` of `tensorflow`'s `Optimzer` to allow for list
+    of tensor to be passed down.
+    """
+
+    def __init__(self, v):
+        self._v = v
+
+    def target(self):
+        return self._v._ref()  # pylint: disable=protected-access
+
+    def update_op(self, optimizer, g_list):
+        update_op = optimizer._apply_dense(g, self._v)  # pylint: disable=protected-access
+        if self._v.constraint is not None:
+            with ops.control_dependencies([update_op]):
+                return self._v.assign(self._v.constraint(self._v))
+        else:
+            return update_op
+
+def _get_processor(v):
+    """The processor of v."""
+    if context.in_eager_mode():
+        return _DenseResourceVariableProcessor(v)
+    if v.op.type == "VarHandleOp":
+        return _DenseResourceVariableProcessor(v)
+    if isinstance(v, variables.Variable):
+        return RefVariableReplicaProcessor(v)
+    if v.op.type == "SubmodelPort":
+        return _StreamingModelPortProcessor(v)
+    raise NotImplementedError("Trying to optimize unsupported type ", v)
+
+class ReplicaOptimizer(Optimizer):
     """ This implements a version of the optimizer that has more than one replica
     that exchange their gradients among one another.
+
+    We override the `Optimizer` class in `tensorflow` in order to wedge in
+    functionality between computation of the gradients (i.e. adding nodes to
+    the computational graph that do this) and the actual application of the
+    gradients for doing the position update.
+
+    This way the position update may actually rely on the gradients of other
+    instances (or any other information) stored in possible replica.
     """
     def __init__(self, use_locking=False, name='ReplicaOptimizer'):
         """ Init function for this class.
@@ -21,7 +67,7 @@ class ReplicaOptimizer(optimizer.Optimizer):
         super(ReplicaOptimizer, self).__init__(use_locking, name)
 
     def compute_and_check_gradients(self, loss, var_list=None,
-                 gate_gradients=optimizer.Optimizer.GATE_OP, aggregation_method=None,
+                 gate_gradients=Optimizer.GATE_GRAPH, aggregation_method=None,
                  colocate_gradients_with_ops=False, grad_loss=None):
         grads_and_vars = self.compute_gradients(
             loss, var_list=var_list, gate_gradients=gate_gradients,
@@ -37,3 +83,65 @@ class ReplicaOptimizer(optimizer.Optimizer):
                 % ([str(v) for _, v in grads_and_vars], loss))
 
         return grads_and_vars
+
+    def apply_gradients(self, grads_and_vars, global_step=None, name=None):
+        """Apply gradients to variables.
+
+        Here, we actually more gradient nodes than we require, namely addtionally
+        those from all other replica. We pick the ones associated with each
+        specific variable, e.g. all gradients to each layer1 weights in all
+        replica. And these are given to the internally called `apply_dense`
+        function.
+
+        :param grads_and_vars: gradients and variables as a list over all parallel
+                        replica
+        :param global_step: global_step node for this replica
+        :param name: name of this operation
+        """
+        grads_and_vars = tuple(grads_and_vars)    # Make sure repeat iteration works.
+        if not grads_and_vars:
+            raise ValueError("No variables provided.")
+        converted_grads_and_vars = []
+        for g, v in grads_and_vars:
+            if g is not None:
+                try:
+                    # Convert the grad to Tensor or IndexedSlices if necessary.
+                    g = ops.convert_to_tensor_or_indexed_slices(g)
+                except TypeError:
+                    raise TypeError(
+                            "Gradient must be convertible to a Tensor"
+                            " or IndexedSlices, or None: %s" % g)
+                if not isinstance(g, (ops.Tensor, ops.IndexedSlices)):
+                    raise TypeError(
+                            "Gradient must be a Tensor, IndexedSlices, or None: %s" % g)
+            p = _get_processor(v)
+            converted_grads_and_vars.append((g, v, p))
+
+        converted_grads_and_vars = tuple(converted_grads_and_vars)
+        var_list = [v for g, v, _ in converted_grads_and_vars if g is not None]
+        if not var_list:
+            raise ValueError("No gradients provided for any variable: %s." %
+                                             ([str(v) for _, _, v in converted_grads_and_vars],))
+        with ops.control_dependencies(None):
+            self._create_slots([_get_variable_for(v) for v in var_list])
+        update_ops = []
+        with ops.name_scope(name, self._name) as name:
+            self._prepare()
+            for grad, var, processor in converted_grads_and_vars:
+                if grad is None:
+                    continue
+                scope_name = var.op.name if context.in_graph_mode() else ""
+                with ops.name_scope("update_" + scope_name), ops.colocate_with(var):
+                    update_ops.append(processor.update_op(self, grad))
+            if global_step is None:
+                apply_updates = self._finish(update_ops, name)
+            else:
+                with ops.control_dependencies([self._finish(update_ops, "update")]):
+                    with ops.colocate_with(global_step):
+                        apply_updates = state_ops.assign_add(global_step, 1, name=name).op
+
+            train_op = ops.get_collection_ref(ops.GraphKeys.TRAIN_OP)
+            if apply_updates not in train_op:
+                train_op.append(apply_updates)
+
+            return apply_updates
