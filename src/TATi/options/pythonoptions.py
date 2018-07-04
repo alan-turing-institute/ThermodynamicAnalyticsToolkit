9b6233c3f9c4a517ef5b8cc16c27cc7e761ad0b2
Frederik Heber <frederik.heber@gmail.com>
2018-07-04 12:41:18 +0200

    Unified PythonOptions with Options.
    - turned descriptions in former MockFlags/PythonOptions into a description
      dict.
    - moreover, default values from __init__() is turned into a default map.
    - __init__() now checks whether all defaults are also described and
      instantiates these with the _options_map.
    - added help() function to give default values, type and description, also
      list of options.

--- src/TATi/options/pythonoptions.py
+++ src/TATi/options/pythonoptions.py
@@ -1,62 +1,113 @@
 from TATi.options.options import Options
 
+import logging
+
 class PythonOptions(Options):
     """ All options that the python interface understands.
-    """
     
+    Example:
+        >>> from TATi.options.pythonoptions import PythonOptions
+        >>> PythonOptions.help()
+        averages_file:             CSV file name to write ensemble averages information such as average kinetic, potential, virial
+        batch_data_file_type:      type of the files to read input from
+         <remainder omitted>
+        >>> PythonOptions.help("batch_data_files")
+        Option name: batch_data_files
+        Description: set of files to read input from
+        Type       : <class 'list'>
+        Default    : []
+        >>> options = PythonOptions(
+        ... batch_data_files=["test.csv"],
+        ... batch_size=3,
+        ... max_steps=1000,
+        ... optimizer="GradientDescent",
+        ... )
     """
-        :param averages_file: csv file name to write averages to
-        :param batch_data_files: set of files to read input from
-        :param batch_data_file_type: type of the files to read input from
-        :param batch_size: The number of samples used to divide sample set into batches in one sampleing step.
-        :param burn_in_steps: number of initial steps to drop when computing averages
-        :param collapse_after_steps: collapse all walkers into a single position again after this many steps
-        :param covariance_blending: mixing for preconditioning matrix to gradient
-                update, identity matrix plus this times the covariance matrix obtained
-                from the other walkers, 0 - will never collapse
-        :param diffusion_map_method:
-        :param do_hessians: whether to add hessian evaluation nodes to graph, used bz optimzier and explorer
-        :param dropout: Keep probability for sampleing dropout, e.g. 0.9
-        :param every_nth: Store only every nth trajectory (and run) point to files, e.g. 10
-        :param fix_parameters: string formatted as "name=value;..." with name of parameter fixed to value
-        :param friction_constant: friction to scale the influence of momenta
-        :param hamiltonian_dynamics_time: number of steps in HMC sampler for checking acceptance criterion
-        :param hidden_activation: Activation function to use for hidden layer: tanh, relu, linear
-        :param hidden_dimension: Dimension of each hidden layer, e.g. 8 8 for two hidden layers each with 8 nodes fully connected
-        :param in_memory_pipeline: whether to feed the dataset from file in-memory (True) or not (False)
-        :param input_columns: Pick a list of the following: (1) x1, (2) x2, (3) x1^2, (4) x2^2, (5) sin(x1), (6) sin(x2).
-        :param input_dimension: number of input nodes/number of features
-        :param inter_ops_threads: size of thread pool used for independent ops
-        :param intra_ops_threads: size of thread pool used for parallelizing an op
-        :param inverse_temperature: Inverse temperature that scales the gradients
-        :param learning_rate: learning rate used during optimization, see also `step_width`
-        :param loss: Set the loss to be measured during sampling, e.g. mean_squared, log_loss, ...
-        :param max_steps: Number of steps to run sampleer.
-        :param number_of_eigenvalues:
-        :param number_walkers: number of dependent walkers to activate ensemble preconditioning
-        :param optimizer: Choose the optimizer to use for sampling: GradientDescent
-        :param output_activation: Activation function to use for output layer: tanh, relu, linear
-        :param output_dimension: number of output nodes/number of classes
-        :param parse_parameters_file: parse neural network parameters from this file on network creation
-        :param parse_steps: indicates the row (in column "step") of the parse_parameters_file to use
-        :param prior_factor: factor for scaling prior force
-        :param prior_lower_boundary: lower boundary for wall-repelling prior force
-        :param prior_power: power of distance used in calculating force
-        :param prior_upper_boundary: upper boundary for wall-repelling prior force
-        :param progress: activate progress bar to show remaining time
-        :param restore_model: Restore model (weights and biases) from a file.
-        :param run_file: CSV run file name to runtime information such as output accuracy and loss values.
-        :param sampler: Choose the sampler to use for sampling: GeometricLangevinAlgorithm_1stOrder, GeometricLangevinAlgorithm_2ndOrder, StochasticGradientLangevinDynamics
-        :param save_model: Save model (weights and biases) to a file for later restoring.
-        :param seed: Seed to use for random number generators.
-        :param sigma: Scale of noise injected to momentum per step for CCaDL.
-        :param sigmaA: Scale of noise in convex combination for CCaDL.
-        :param step_width: step width \Delta t to use, e.g. 0.01
-        :param summaries_path: path to write summaries (for TensorBoard) to
-        :param trajectory_file: CSV file name to output trajectories of sampling, i.e. weights and evaluated loss function.
-        :param use_reweighting:
-        :param verbose: how much (debugging) information to print
-    """
+
+    _description_map = {
+        "averages_file": "CSV file name to write ensemble averages information "+ \
+                         "such as average kinetic, potential, virial",
+        "batch_data_files": "set of files to read input from",
+        "batch_data_file_type": "type of the files to read input from",
+        "batch_size": "The number of samples used to divide sample set into "+ \
+                      "batches in one sampleing step.",
+        "burn_in_steps": "number of initial steps to drop when computing averages",
+        "collapse_after_steps": "collapse all walkers into a single position "+ \
+                                "again after this many steps",
+        "covariance_blending": "mixing for preconditioning matrix to gradient "+ \
+                               "update, identity matrix plus this times the covariance matrix "+
+                               "obtained from the other walkers, 0 - will never collapse",
+        "diffusion_map_method": "name of method to use for diffusion map "+ \
+                                "analysis: vanilla, TMDap, pydiffmap",
+        "do_hessians": "whether to add hessian evaluation nodes to graph, used "+ \
+                       "by optimzier and explorer",
+        "dropout": "Keep probability for sampleing dropout, e.g. 0.9",
+        "every_nth": "Store only every nth trajectory (and run) point to files, "+ \
+                     "e.g. 10",
+        "fix_parameters": "string formatted as 'name=value;...' with name of "+ \
+                          "parameter fixed to value",
+        "friction_constant": "friction to scale the influence of momenta",
+        "hamiltonian_dynamics_time": "time w.r.t step_width for HMC sampler "+ \
+                                     "passing between checking acceptance "+ \
+                                     "criterion",
+        "hidden_activation": "Activation function to use for hidden layer: "+ \
+                             "tanh, relu, linear",
+        "hidden_dimension": "Dimension of each hidden layer, e.g. 8 8 for two "+ \
+                            "hidden layers each with 8 nodes fully connected",
+        "in_memory_pipeline": "whether to feed the dataset from file in-memory "+ \
+                              "(True) or not (False)",
+        "input_columns": "Pick a list of the following: "+ \
+                         "(1) x1, (2) x2, (3) x1^2, (4) x2^2, (5) sin(x1), "+ \
+                         "(6) sin(x2).",
+        "input_dimension": "number of input nodes/number of features",
+        "inter_ops_threads": "size of thread pool used for independent ops",
+        "intra_ops_threads": "size of thread pool used for parallelizing an op",
+        "inverse_temperature": "Inverse temperature that scales the gradients",
+        "learning_rate": "learning rate used during optimization, see also "+ \
+                         "`step_width`",
+        "loss": "Set the loss to be measured during sampling, e.g. "+ \
+                "mean_squared, log_loss, ...",
+        "max_steps": "Number of steps to run sampleer.",
+        "number_of_eigenvalues": "",
+        "number_walkers": "number of dependent walkers to activate ensemble "+ \
+                          "preconditioning",
+        "optimizer": "Choose the optimizer to use for sampling: GradientDescent",
+        "output_activation": "Activation function to use for output layer: "+ \
+                             "tanh, relu, linear",
+        "output_dimension": "number of output nodes/number of classes",
+        "parse_parameters_file": "parse neural network parameters from this "+ \
+                                 "file on network creation",
+        "parse_steps": "indicates the row (in column 'step') of the "+ \
+                       "parse_parameters_file to use",
+        "prior_factor": "factor for scaling prior force",
+        "prior_lower_boundary": "lower boundary for wall-repelling prior force",
+        "prior_power": "power of distance used in calculating force",
+        "prior_upper_boundary": "upper boundary for wall-repelling prior force",
+        "progress": "activate progress bar to show remaining time",
+        "restore_model": "Restore model (weights and biases) from a file.",
+        "run_file": "CSV run file name to runtime information such as output "+ \
+                    "accuracy and loss values.",
+        "sampler": "Choose the sampler to use for sampling: "+ \
+                   "BAOAB,"+ \
+                   "CovarianceControlledAdaptiveLangevin, "+ \
+                   "GeometricLangevinAlgorithm_1stOrder, "+ \
+                   "GeometricLangevinAlgorithm_2ndOrder, "+ \
+                   "HamiltonianMonteCarlo, "+ \
+                   "StochasticGradientLangevinDynamics",
+        "save_model": "Save model (weights and biases) to a file for later "+ \
+                      "restoring.",
+        "seed": "Seed to use for random number generators.",
+        "sigma": "Scale of noise injected to momentum per step for CCaDL.",
+        "sigmaA": "Scale of noise in convex combination for CCaDL.",
+        "sql_db": "Filename of sqlite3 database file to write iteration "+ \
+                  "information to",
+        "step_width": "step width \Delta t to use, e.g. 0.01",
+        "summaries_path": "path to write summaries (for TensorBoard) to",
+        "trajectory_file": "CSV file name to output trajectories of sampling, "+ \
+                           "i.e. weights and evaluated loss function.",
+        "use_reweighting": "",
+        "verbose": "how much (debugging) information to print",
+    }
     _default_map = {
         "averages_file": None,
         "batch_data_files": [],
@@ -102,18 +153,48 @@ class PythonOptions(Options):
         "seed": None,
         "sigma": 1.,
         "sigmaA": 1.,
+        "sql_db": None,
         "step_width": 0.1,
         "summaries_path": None,
         "trajectory_file": None,
         "use_reweighting": False,
-        "verbose": 0    }
+        "verbose": 0
+    }
+
+    @staticmethod
+    def help(key=None):
+        """ Prints help for each option or all option names if key is None
+
+        :param key: name of option or None for list of options
+        """
+        if key is None:
+            max_length = 0
+            for key in PythonOptions._description_map.keys():
+                if len(key) > max_length:
+                    max_length = len(key)
+            for key in sorted(PythonOptions._description_map.keys()):
+                print(str(key)+": "+" "*(max_length-len(key))+PythonOptions._description_map[key])
+        elif key in PythonOptions._description_map.keys():
+            print("Option name: "+str(key))
+            print("Description: "+PythonOptions._description_map[key])
+            print("Type       : "+str(type(PythonOptions._default_map[key])))
+            print("Default    : "+str(PythonOptions._default_map[key]))
 
     def __init__(self, *args, **kwargs):
         """ Init function to set various default values.
 
         """
+        # make sure all keys are described
         super(PythonOptions, self).__init__()
+        self._excluded_keys.append("_description_map")
         self._excluded_keys.append("_default_map")
+        for key in self._default_map.keys():
+            if key not in  self._description_map.keys():
+                logging.error("Option "+str(key)+" missing in _description_map")
+        for key in self._description_map.keys():
+            if key not in self._default_map.keys():
+                logging.error("Option " + str(key) + " missing in _default_map")
+        assert ( self._default_map.keys() == self._description_map.keys() )
         for key in self._default_map.keys():
             self.add(key)
             if key in kwargs:
