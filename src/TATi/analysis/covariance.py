ef6d04cfa372f31c5f00708b6eaa7008be96a447
Frederik Heber <frederik.heber@gmail.com>
2018-12-20 15:29:08 +0100

    Changes to covariance for multiple walker.

--- src/TATi/analysis/covariance.py
+++ src/TATi/analysis/covariance.py
@@ -10,42 +10,51 @@ class Covariance(object):
     """
     def __init__(self, trajectory):
         if isinstance(trajectory, ParsedTrajectory):
-            self.trajectory = trajectory.get_trajectory()
+            self._trajectory = trajectory.get_trajectory()
         else:
-            self.trajectory = trajectory
-        self.number_degrees = self.trajectory.shape[1]
+            self._trajectory = trajectory
+        self._number_degrees = self._trajectory.shape[1]
         self.covariance = None
+        self.values = None
+        self.vectors = None
 
-    def _setup_covariance(self):
-        if self.covariance is None:
-            self.covariance = np.cov(self.trajectory, rowvar=False)
+    @staticmethod
+    def _setup_covariance(trajectory):
+        return np.cov(trajectory, rowvar=False)
 
     def compute(self, number_eigenvalues):
-        self._setup_covariance()
         # set up covariance matrix
-        max_rank = min(self.covariance.shape[0], self.covariance.shape[1])
+        if self.covariance is None:
+            self.covariance = self._setup_covariance(self._trajectory)
+        if self.values is None or self.vectors is None:
+            self.values, self.vectors = self._compute_eigendecomposition(
+                number_eigenvalues=number_eigenvalues, covariance=self.covariance)
+
+    @staticmethod
+    def _compute_eigendecomposition(number_eigenvalues, covariance):
+        max_rank = min(covariance.shape[0], covariance.shape[1])
         if number_eigenvalues is not None and number_eigenvalues < max_rank:
             # sparse eigenvectors
-            w, v = sps.linalg.eigs(self.covariance, k=number_eigenvalues)
+            w, v = sps.linalg.eigs(covariance, k=number_eigenvalues)
         else:
             # full eigenvectors
-            w, v = np.linalg.eig(self.covariance)
+            w, v = np.linalg.eig(covariance)
         ix = w.argsort()[::-1]
         # covariance is a symmetric real-valued matrix, hence has real-valued
         # eigensystem
-        self.vectors = np.real(v[:, ix])
-        self.values = np.real(w[ix])
+        vectors = np.real(v[:, ix])
+        values = np.real(w[ix])
+        return values, vectors
 
     def write_covariance_as_csv(self, filename):
-        self._setup_covariance()
-        header = [("c%d" % (i)) for i in range(self.number_degrees)]
+        header = [("c%d" % (i)) for i in range(self._number_degrees)]
         if filename is not None:
             np.savetxt(filename, self.covariance, delimiter=",", header=",".join(header), comments="")
 
     def write_vectors_as_csv(self, filename):
         # we write the vectors as transposed to have them as column vectors
         if filename is not None:
-            header = [("c%d" % (i)) for i in range(self.number_degrees)]
+            header = [("c%d" % (i)) for i in range(self._number_degrees)]
             np.savetxt(filename, self.vectors.T, delimiter=",", header=",".join(header), comments="")
 
     def write_values_as_csv(self, filename):
